174ed29f9c6712adb9562b0dd68c499587e2874c
SwiftGuard1 <79541415+SwiftGuard1@users.noreply.github.com>
2023-01-26 19:10:12 -0500

    Main window created

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const gameWindow = document.createElement("div")
+const gameWindow = document.createElement("div");
+gameWindow.style.width = "400px";
+gameWindow.style.height = "400px";
+gameWindow.style.border = "2px solid darkgrey";
+gameWindow.style.position = "fixed";
+gameWindow.style.top = "10px";
+gameWindow.style.right = "10px";
+gameWindow.style.backgroundSize = "100px 100px";
+gameWindow.style.backgroundRepeat = "repeat";
+gameWindow.style.backgroundImage = "url('https://media.istockphoto.com/id/653967114/vector/hammer-and-sickle-seamless-pattern.jpg?s=612x612&w=0&k=20&c=R4104ZT0uIniV9_kVe-xT8FBIGoWFBb6IGZOTtIvj8U=')";
+document.body.appendChild(gameWindow);
