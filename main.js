d2badb812aee35a9af6fb288b232c95db6543e07
SwiftGuard1 <79541415+SwiftGuard1@users.noreply.github.com>
2023-01-26 19:37:35 -0500

    Added nuke

--- main.js
+++ main.js
@@ -10,6 +10,11 @@ gameWindow.style.backgroundRepeat = "repeat";
 gameWindow.style.backgroundImage = "url('https://media.istockphoto.com/id/653967114/vector/hammer-and-sickle-seamless-pattern.jpg?s=612x612&w=0&k=20&c=R4104ZT0uIniV9_kVe-xT8FBIGoWFBb6IGZOTtIvj8U=')";
 
 const nuke = document.createElement("img");
+nuke.style.width = "350px";
+nuke.style.height = "250px";
+nuke.style.position = "fixed";
+nuke.style.top = "75px";
+nuke.style.right = "35px";
 nuke.src = "https://www.brusselsreport.eu/wp-content/uploads/2022/05/atomic-bomb-g37cdc76d2_1280.png";
 
 const nukeWarning = document.createElement("img");
@@ -22,3 +27,4 @@ const destination = document.createElement("img");
 destination.src = "https://media.istockphoto.com/id/1281603402/photo/north-america-from-space.jpg?s=612x612&w=0&k=20&c=zpcVWdAidlU2BXXuXUgmHhc-8RPerStelip32HkJOBM=";
 
 document.body.appendChild(gameWindow);
+document.body.appendChild(nuke);
